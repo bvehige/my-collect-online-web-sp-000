2f3372388b53b0668593052dd955e393e7fd3a81
Brian Vehige <brianvehige@gmail.com>
2020-02-21 09:18:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,10 @@
 def my_collect(array)
-   if block_given?
   i = 0
-  new_collection =[]
  while i < array.length 
  yield array[i]
- new_collection.push
   i = i + 1 
 end
- new_collection.collect
+ 
  
  else 
   array.collect
